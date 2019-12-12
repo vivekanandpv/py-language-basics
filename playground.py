5a82201dc94c8ff43fd5060d11664ff24528254e
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 22:50:52 +0530

    done

--- playground.py
+++ playground.py
@@ -1,71 +1,25 @@
-vowels = []
+# tuples are immutable lists for practical considerations
+# duplicates may exist
+vowels = ('a', 'e', 'i', 'o', 'u')
 
-# Add items
-vowels.append('a')
-vowels.append('e')
-vowels.append('i')
-vowels.append('o')
-vowels.append('u')
+# to create a tuple of only one element,
+# add trailing comma x = ('el',)
 
-print(f'vowels original: {vowels}')
+# please note, tuples cannot add, remove, mutate
+# but a tuple variable can get a new reference
+# akin to string assignments
 
-# Mutation
-vowels[0] = 'A'
-print(f'vowels after mutation: {vowels}')
+# del deletes the whole tuple
+# a new tuple can be created by joining two tuples
+numbers = (0, 1, 2, 3)
 
-# Existence
-if 'A' in vowels:
-    print('A is found')
-else:
-    print('A is not found')
+new_tuple = numbers + vowels
+print(f'new_tuple: {new_tuple}')
 
-# Insert at an index
-# right shifts
-# if index is > length, no error thrown,
-# but element is inserted at the end
-vowels.insert(1, '?')
-print(f'vowels after insert: {vowels}')
 
-# remove by element
-vowels.remove('?')
-print(f'vowels after remove: {vowels}')
+# existence check
+if 'a' in vowels:
+    print('a is found in the tuple')
 
-# pop
-# end is presumed
-# out of index is an error (IndexError)
-vowels.pop(4)
-print(f'vowels after pop: {vowels}')
-
-# del
-# del list deletes the list completely
-# (object is deleted from memory)
-del vowels[0]
-print(f'vowels after del: {vowels}')
-
-# clear - empties the list
-vowels.clear()
-print(f'vowels after clear: {vowels}')
-
-# copy - create a shallow copy consider for value types
-# in case the list contains objects,
-# only references are copied
-# x = y is just the reference copy
-x = [1, 2, 3, 4]
-y = x.copy()  # shallow copy, can consider y = x[:]
-x[2] = 1000  # till this point x and y are the same reference!
-print(f'Are x and y same reference? {x == y}')
-print(f'list x: {x}')
-print(f'list y: {y}')
-
-# joining two lists
-a = [1, 2, 3]
-b = [4, 5, 6]
-
-c = a + b
-print(f'a + b = {c}')
-
-# extending a list
-a.extend(b)
-print(f'a after extending: {a}')
-
-# please check sort and reverse
+# len() operator
+print(f'length of the tuple: {len(vowels)}')
