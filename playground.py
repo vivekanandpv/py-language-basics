97e46ef0c5926a7efb06f60170f4cf962bdaa643
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 18:32:36 +0530

    done

---
+++ playground.py
@@ -0,0 +1,11 @@
+x = 100
+
+# No switch construct in Python
+if x > 100:
+    print(f'x > {100}')
+elif x > 70:    # not else if
+    print(f'x > {70}')
+else:
+    print(f'x <= {70}')
+
+# In any suite, pass can be used to take no action
