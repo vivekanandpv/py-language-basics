b0855e43943e810fbd58fb1b716c32d8763af103
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-12 22:40:47 +0530

    done

--- playground.py
+++ playground.py
@@ -1,23 +1,71 @@
-# list is ordered collection that supports change
+vowels = []
 
-vowels = ['a', 'e', 'i', 'o', 'u']
+# Add items
+vowels.append('a')
+vowels.append('e')
+vowels.append('i')
+vowels.append('o')
+vowels.append('u')
 
-# Accessing by index
-print(f'[2]: {vowels[2]}')  # i
+print(f'vowels original: {vowels}')
 
-# Syntax [-x] considers from the end.
-# -1 is end -0 is beginning!
-# if x exceeds the length (both [x] and [-x]),
-# IndexError is thrown
+# Mutation
+vowels[0] = 'A'
+print(f'vowels after mutation: {vowels}')
 
-# Selection
-print(f'[0:3] {vowels[0:3]}')  # a, e, i
+# Existence
+if 'A' in vowels:
+    print('A is found')
+else:
+    print('A is not found')
 
-# Syntax: [x:y]
-# where x is the starting index (included)
-# y is the ending index (not included)
-# mathematically: [x, y)
-# if x is absent, 0 is default
-# if y is absent, end is presumed
-# if x or y or both are > length, there is error
-# but the result is empty
+# Insert at an index
+# right shifts
+# if index is > length, no error thrown,
+# but element is inserted at the end
+vowels.insert(1, '?')
+print(f'vowels after insert: {vowels}')
+
+# remove by element
+vowels.remove('?')
+print(f'vowels after remove: {vowels}')
+
+# pop
+# end is presumed
+# out of index is an error (IndexError)
+vowels.pop(4)
+print(f'vowels after pop: {vowels}')
+
+# del
+# del list deletes the list completely
+# (object is deleted from memory)
+del vowels[0]
+print(f'vowels after del: {vowels}')
+
+# clear - empties the list
+vowels.clear()
+print(f'vowels after clear: {vowels}')
+
+# copy - create a shallow copy consider for value types
+# in case the list contains objects,
+# only references are copied
+# x = y is just the reference copy
+x = [1, 2, 3, 4]
+y = x.copy()  # shallow copy, can consider y = x[:]
+x[2] = 1000  # till this point x and y are the same reference!
+print(f'Are x and y same reference? {x == y}')
+print(f'list x: {x}')
+print(f'list y: {y}')
+
+# joining two lists
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+c = a + b
+print(f'a + b = {c}')
+
+# extending a list
+a.extend(b)
+print(f'a after extending: {a}')
+
+# please check sort and reverse
