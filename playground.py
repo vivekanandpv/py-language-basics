c657f8a9384eb4d3802ba098eac154a0d08b4716
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-13 22:25:08 +0530

    done

--- playground.py
+++ playground.py
@@ -1,16 +1,11 @@
-class Car:
-    # __init__ is not a constructor
-    # constructor is provided by the runtime
-
-    # all instance methods should take self
-    # as the first argument
-    # __init__ should set class invariants
-    # that are fixed for the life-time of the object
+class Vehicle:
+    # everything is inherited
+    # inheritance is used to reuse implementation
     def __init__(self, name='Default'):
-        # no access modifiers
-        # everything is public
-        # implementations typically have _
         self._name = name
+
+
+class Car(Vehicle):
 
     def get_name(self):
         return self._name
