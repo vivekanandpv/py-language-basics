cd9a396085edb047a065878878dd143e035a5d9c
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-13 21:55:11 +0530

    done

--- playground.py
+++ playground.py
@@ -1,7 +1,19 @@
 class Car:
-    pass
+    # __init__ is not a constructor
+    # constructor is provided by the runtime
+
+    # all instance methods should take self
+    # as the first argument
+
+    def __init__(self, name='Default'):
+        self._name = name
+
+    def get_name(self):
+        return self._name
 
 
-car_one = Car()  # calling the constructor function
+car_one = Car()
+car_two = Car('MG Hector')
 
-print(f'car_one: {type(car_one)}')
+print(f'car_one.get_name(): {car_one.get_name()}')
+print(f'car_two.get_name("MG Hector"): {car_two.get_name()}')
