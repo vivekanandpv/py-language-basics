3a401cfa2f2d4f7a351204cdb66bda2e1147f9e8
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 16:34:13 +0530

    done

--- playground.py
+++ playground.py
@@ -1,13 +1,14 @@
-# Syntax [expr(item) for item in iterable]
+# Syntax {expr(item) for item in iterable}
+# no duplications
 
 from math import factorial
 words = 'I am here to learn list comprehensions'.split()
 
-print([len(word) for word in words])
+print({len(word) for word in words})
 
-factorials = [factorial(n) for n in range(20)]
+factorials = {factorial(n) for n in range(20)}
 
-print([len(str(item)) for item in factorials])
+print({len(str(item)) for item in factorials})
 
 # or
-print([len(str(factorial(n))) for n in range(20)])
+print({len(str(factorial(n))) for n in range(20)})
