aef7d26cebb759b2cb08d4ced9fe5c37d5462d92
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 17:23:08 +0530

    done

--- playground.py
+++ playground.py
@@ -1,2 +1,39 @@
-from math import factorial
-print({len(str(factorial(n))) for n in range(20) if (n % 2 == 0)})
+def range_clone(start=0, end=10):
+
+    index = start
+
+    while index <= end:
+        yield index
+        index += 1
+
+
+for i in range_clone(end=20):
+    print(f'range_clone: {i}')
+
+
+def fibonacci(start=1, end=100):
+    a = start
+    b = start
+
+    while b < end:
+        a, b = b, a + b
+        yield a
+
+
+for i in fibonacci(end=20):
+    print(f'Fibonacci: {i}')
+
+
+def get_distinct(iterable):
+    items = set()
+
+    for item in iterable:
+        if item in items:
+            continue
+        else:
+            yield item
+            items.add(item)
+
+
+for i in get_distinct([1, 2, 3, 4, 5, 4, 3, 2, 1, 6, 7, 8]):
+    print(f'distinct: {i}')
