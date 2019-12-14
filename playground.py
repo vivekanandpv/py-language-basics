85f493e993fde4c49a11f24d504f95b96765d625
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 17:57:02 +0530

    done

--- playground.py
+++ playground.py
@@ -1,63 +1,8 @@
-# generators are lazy
-# if next() is applied at end,
-# StopIterationError is raised
-# generators can be infinite
+generator_comprehension = ((n, n*n) for n in range(100) if (n % 3 == 0))
 
+# <generator object <genexpr> at 0x000...>
+print(generator_comprehension)
 
-def range_clone(start=0, end=10):
-
-    index = start
-
-    while index <= end:
-        yield index
-        index += 1
-
-
-for i in range_clone(end=20):
-    print(f'range_clone: {i}')
-
-
-def fibonacci(start=1, end=100):
-    a = start
-    b = start
-
-    while b < end:
-        a, b = b, a + b
-        yield a
-
-
-for i in fibonacci(end=20):
-    print(f'Fibonacci: {i}')
-
-
-def get_distinct(iterable):
-    items = set()
-
-    for item in iterable:
-        if item in items:
-            continue
-        else:
-            yield item
-            items.add(item)
-
-
-for i in get_distinct([1, 2, 3, 4, 5, 4, 3, 2, 1, 6, 7, 8]):
-    print(f'distinct: {i}')
-
-
-def fibonacci_infinite(start=1):
-    a = start
-    b = start
-
-    while True:
-        a, b = b, a + b
-        yield a
-
-
-for i in fibonacci_infinite():
-
-    if i > 1000:
-        print('Fibonacci infinite ended')
-        break
-    else:
-        print(f'Fibonacci infinite: {i}')
+for i, j in generator_comprehension:
+    # evaluation starts from this point
+    print(f'generator comprehension: {i}: sq: {j}')
