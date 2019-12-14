a06684f6d02c2755dde0f5d36f3cde51a32fd9b6
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 18:01:27 +0530

    done

--- playground.py
+++ playground.py
@@ -6,3 +6,8 @@ print(generator_comprehension)
 for i, j in generator_comprehension:
     # evaluation starts from this point
     print(f'generator comprehension: {i}: sq: {j}')
+
+
+# load is significantly reduced
+cube_generator = (n ** 3 for n in range(1000000))
+print(f'sum of cubes: {sum(cube_generator)}')
