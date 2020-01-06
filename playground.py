5a8045cd939d3d2a014dc8c3db0f00f897f3952e
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-06 12:50:08 +0530

    solution

--- playground.py
+++ playground.py
@@ -3,3 +3,22 @@
 # Create a function that takes arbitrary arguments of numbers
 
 # This function should return the sum of squares of only odd numbers passed to it
+
+
+# Solution:
+
+# We take arbitrary arguments as *numbers
+def do_work(*numbers):
+    # numbers is a tuple inside
+    sum = 0
+
+    for n in numbers:
+        if n % 2 != 0:
+            sum += n**2
+
+    return sum
+
+
+print(do_work(1, 2, 3, 4, 5, 6, 7))
+print(do_work())
+print(do_work(2, 4, 6, 8, 1))
