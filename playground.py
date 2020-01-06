e9f97a582edb0a6c80331ffd1139ebd0afebf121
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-06 16:51:17 +0530

    done

--- playground.py
+++ playground.py
@@ -1,26 +1,7 @@
-# Decorators are higher order functions
-# Decorator takes an existing function
-# and returns another function
-# This way, the existing function can be extended
+# Exercise:
 
+# create a function get_amount, which returns a decimal number.
 
-def upper_case(f):
-    print(f'upper_case called {f}')
-
-    def wrapper(*args, **kwargs):
-        print(f'upper_case wrapper called args: {args} kwargs: {kwargs}')
-        result = f(*args, **kwargs)
-        print(f'wrapper got: {result}, and returning')
-        return result.upper()
-
-    print('upper_case returning')
-    return wrapper
-
-
-@upper_case
-def get_city(x, y):
-    print('get_city called')
-    return 'bengaluru'
-
-
-print(f'final result: {get_city("value of x", y=1200)}')
+# create a decorator which extends get_amount by:
+# 1.	applying indian rupee symbol
+# 2.	rounding off decimal places to 2
