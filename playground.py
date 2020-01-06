4fc806c312f132984c05e92cf040b6b0759dcacf
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-06 12:44:50 +0530

    exercise

--- playground.py
+++ playground.py
@@ -1,31 +1,5 @@
-# default parameters
-def foo(year=2019):
-    year += 1
-    return year
+# Exercise:
 
+# Create a function that takes arbitrary arguments of numbers
 
-print(foo())
-
-# keyword arguments
-
-
-def bar(year, month, day):
-    print(f'year: {year}, month: {month}, day: {day}')
-
-
-# non keyword argument should not come after keyword argument
-# bar(month=8, 2019, 1) is SyntaxError
-bar(day=10, year=2019, month=7)
-
-# arbitrary arguments
-# arguments become a tuple
-# * should come at the end
-
-
-def baz(first, *args):
-    print(f'first: {first}')
-    for arg in args:
-        print(f'arg: {arg}')
-
-
-baz(2019, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+# This function should return the sum of squares of only odd numbers passed to it
