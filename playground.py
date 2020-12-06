956b7857089ea93b616145cc1dcbd69e2fd342cd
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-06 16:43:04 +0530

    done

--- playground.py
+++ playground.py
@@ -1,43 +1,83 @@
-#   Strings can be written in single or double quotes.
-#   There is no character type.
-name = 'Wilson Jacob'
-city = "Wisconsin"
+#   documentation: https://docs.python.org/3.4/library/stdtypes.html#string-methods
 
-#   String concatenation
-print('Name: ' + name + '; city: ' + city)
+sample = 'this is a sample text for you'
 
-#   Formatted strings
-print(F'Name: {name}; city: {city}')  # F is also allowed, but f is conventionally used
+#   Capitalizes the first character of the string
+print(sample.capitalize())
 
-#   Multiline strings
-print('''This is a multiline
-note for all the students
-of computer science.''')  # You can use double quotes instead
+#   Use casefold for case-agnostic matching
+print('First'.casefold() == 'fiRst'.casefold())
 
-#   Unicode is directly supported in Python 3.x
-print('''नमस्ते''')
+#   Use center to get the string padded with character of choice (space is default)
+#   second parameter should be exactly 1 character
+#   This has left-bias (play with even and odd width here)
+print('Topic'.center(12, '~'))
 
-#   You can also use unicode code units
-#   This is Python 2.x style
-print(u'\u265E')  # Chess black night
+#   count returns the number of non-overlapping occurances of a substring
+print('The name that can be named is not the eternal name'.count('name'))
 
-#   In Python 3.x, we can directly embed the character we want
-print('♞')
+#   endswith and startswith
+print('CustomerController'.endswith('Controller'))
+print('CustomerController'.startswith('Customer'))
 
-#   Python 2.x formatted strings (uses indexes)
-print('Name: {0}; city: {1}'.format(name, city))
+#   find the substring
+#   -1 if not found
+#   The default direction is from left
+#   rfind and rindex work from right side
+print('He went on to become a good engineer'.find('good'))
 
-#   Other variants
+#   index is like find, but raises ValueError if substring is not found
+print('He went on to become a good engineer'.index('good'))
 
-#   without indexes
-print('Name: {}; city: {}'.format(name, city))
+#   to check if the string is alpha-numeric (no special characters)
+print('ABCD1234'.isalnum())
 
-#   named placeholders
-print('Name: {user_name}; city: {user_city}'.format(user_name=name, user_city=city))
+#   to check if the string is all characters (no special characters including space!)
+print('I drive a car'.isalpha())  # False because of spaces!
+print('UnitedStatesOfAmerica'.isalpha())
 
-#   Providing other formatting
-pi = 3.141592635
-print('π = {math_pi:.4f}'.format(math_pi=pi))
+#   to check if the string contains numbers
+#   these functions are almost equal for general use
+#   but the when unicode-specific numerics are used they behave differently
+print('124'.isnumeric())
+print('124'.isdigit())
+print('124'.isdecimal())
 
-#   Some useful formatting types:
-#   https://docs.python.org/3.4/library/string.html#format-specification-mini-language
+#   ½ is one character
+print('½'.isnumeric())  # True
+print('½'.isdigit())  # False
+print('½'.isdecimal())  # False
+
+#   ³ is superscript
+print('³'.isnumeric())  # True
+print('³'.isdigit())  # True
+print('³'.isdecimal())  # False
+
+#   to check the case
+print('all lowercase letters'.islower())
+print('ALL UPPERCASE LETTERS'.isupper())
+print('This Follows Title Case Convention'.istitle())
+
+#   case conversion
+print('To LoWeRcAsE'.lower())
+print('tO uPpErCase'.upper())
+print('tO tiTLE'.title())
+
+#   splitting
+print('A B C D E F'.split())  # splits at space
+print('A;B;C;D;E;F'.split(';'))  # splits at delimiter ;
+print('A;B;C;D;E;F'.split(';', maxsplit=2))  # splits at delimiter ; only two splits
+
+print('''This is a 
+multiline
+string'''.splitlines())  # splits at newline
+
+#   stripping the whitespace at both ends
+print('   Did I strip the whitespace?    '.strip())
+
+#   zero-padding to the left
+print('3.141592635'.zfill(12))
+
+print('Hi there!'.zfill(15))    # doesn't make sense, but still works
+
+
